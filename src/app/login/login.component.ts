ac2e069ab40f9874128f6c00a3ce132d2e856f2e
joshua laver <joshua.laver@sonraisecurity.com>
2020-02-22 18:15:05 -0400

    feat: Login Working | TODO CSS | TODO ERROR

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup; 
 
-  ngOnInit(): void {
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+    ) { 
   }
 
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  submit(){
+    if(this.loginForm.valid){
+      console.log("Valid");
+
+      const username = this.loginForm.get('username').value;
+      console.log(username);
+
+      if(username === "teacher"){
+        console.log("Yes: Teacher");
+        this.router.navigateByUrl('/teacher');
+        
+      }
+      else if (username === "student"){
+        console.log("Yes: Student");
+        this.router.navigateByUrl('/students')
+        
+      }
+    }
+    else {
+      console.log("Unvalid");
+      //TODO ERROR MESSAGE 
+    }
+  }
 }
