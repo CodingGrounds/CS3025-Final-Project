671dd0366fab6d65efef226686906d775b698ad9
Jason Cleveland <jasonncleveland@gmail.com>
2020-02-25 09:35:27 -0400

    Add Lesson Quiz Page
    This page will display the quizzes for a lesson. They are split into
    either multiple choice or fill in the blanks format.

--- src/app/students/quiz/multiple-choice/multiple-choice.component.ts
+++ src/app/students/quiz/multiple-choice/multiple-choice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-multiple-choice',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MultipleChoiceComponent implements OnInit {
 
+  @Input() quiz: any;
+
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.quiz);
   }
-
 }
